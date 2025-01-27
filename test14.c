e0fb737c9dbe6766f843aa6b35ad76e38890f6a2
wujianyu <wujianyu322@gmail.com>
2025-01-27 12:13:33 +0800

    列印出自己的名子

---
+++ test14.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char name[] = {'J','A','I','N'};
+    int i;
+
+    for ( i = 0; i < 4; i++ )
+        printf("%c", name[i]);
+        printf("\n");
+
+
+
+
+
+
+ getchar();
+ return 0;
+}
