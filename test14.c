88dc0f9cd24e8eea9d80eed52a20704b66dbb029
wujianyu <wujianyu322@gmail.com>
2025-01-27 14:38:15 +0800

    基本字串的讀取及輸出

--- test14.c
+++ test14.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    char name[] = {'J','A','I','N'};
-    int i;
-
-    for ( i = 0; i < 4; i++ )
-        printf("%c", name[i]);
-        printf("\n");
-
-
+    char str1[15], str2[15], str3[15];
+    printf("請輸入 3 個字串 \n");
+    scanf("%s%s%s",str1, str2, str3 );
+    printf("字串 1 是 ===> %s\n",str1);
+    printf("字串 2 是 ===> %s\n",str2);
+    printf("字串 3 是 ===> %s\n",str3);
 
 
 
