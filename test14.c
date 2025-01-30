ac05dc8dc63f6cf4baa4a15c7977d271d66eca35
wujianyu <wujianyu322@gmail.com>
2025-01-30 12:26:36 +0800

    puts()函數輸出的應用

--- test14.c
+++ test14.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    char str1[15], str2[15], str3[15];
-    printf("請輸入 3 個字串 \n");
-    scanf("%s%s%s",str1, str2, str3 );
-    printf("字串 1 是 ===> %s\n",str1);
-    printf("字串 2 是 ===> %s\n",str2);
-    printf("字串 3 是 ===> %s\n",str3);
+    char str[80] = "Ming-Chi Institute of Technology";
 
-
-
-
- getchar();
+    printf("字串輸出如下 \n");
+    puts(str);
+    puts(str+4);
+    puts(&str[4]);
+ getchar();  
  return 0;
 }
