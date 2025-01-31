5e23f48a282b9496cb201149a1eac138411a5757
wujianyu <wujianyu322@gmail.com>
2025-01-31 14:54:52 +0800

    字串大小寫轉變

--- test14.c
+++ test14.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+
 int main()
 {
-    char str1[] = "This is a good book for C";
-    char str2[] = "Introduction to C";
-
-    puts("呼叫 strcpy 前");
-    printf("str1 = %s\n",str1);
-    printf("str2 = %s\n",str2);
-    strcpy(str1,str2);
-    puts("呼叫 strcpy 後");
-    printf("str1 = %s\n",str1);
-    printf("str2 = %s\n",str2);
- getchar();  
- return 0;
+    char code[] = "Ming52Chi";
+    printf("原始驗證碼 = %s\n", code);
+    strupr(code);
+    printf("大寫驗證碼 = %s\n", code);
+    strlwr(code);
+    printf("小寫驗證碼 = %s\n", code);
+    getchar();  
+    return 0;
 }
