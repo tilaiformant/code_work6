24073c841042d252e57cf3c3b4512f06dfb10649
wujianyu <wujianyu322@gmail.com>
2025-02-03 12:42:19 +0800

    輸入帳號和密碼

--- test14.c
+++ test14.c
@@ -2,15 +2,25 @@
 #include <string.h>
 int main()
 {
-   char fruit[][10] = {"Apple",
-                        "Banana",
-                        "Grapes"};
-   int i;
-   for ( i = 0; i < 3; i++ )
+   char account[] = "shios";
+   char password[] = "jay";
+   char acc[10];
+   char pass[10];
+   printf("請輸入帳號 : ",acc);
+   gets(acc);
+   printf("請輸入密碼 : ",pass);
+   gets(pass);
+   if (strcmp(account, acc) ==0 )
    {
-      printf("字串內容 %s\n",fruit[i]);
-      printf("字串位址 %x\n",fruit[i]);
+      if (strcmp(password, pass) ==0 )
+         printf("歡迎進入Deepmind系統\n");
+      else
+         printf("密碼錯誤\n");
+
    }
+   else
+      printf("帳號錯誤\n");
+
 
    getchar();
    return 0;
