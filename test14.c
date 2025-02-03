76383411e793fc07543858a3daef1d58dc954521
wujianyu <wujianyu322@gmail.com>
2025-02-03 13:28:17 +0800

    建立銀行密碼

--- test14.c
+++ test14.c
@@ -2,25 +2,17 @@
 #include <string.h>
 int main()
 {
-   char account[] = "shios";
-   char password[] = "jay";
-   char acc[10];
-   char pass[10];
-   printf("請輸入帳號 : ",acc);
-   gets(acc);
-   printf("請輸入密碼 : ",pass);
-   gets(pass);
-   if (strcmp(account, acc) ==0 )
-   {
-      if (strcmp(password, pass) ==0 )
-         printf("歡迎進入Deepmind系統\n");
-      else
-         printf("密碼錯誤\n");
-
-   }
+   char password[12];
+   int len;
+   printf("建立密碼 : ");
+   scanf("%s", password);
+   len = strlen(password);
+   if ( len > 10 )
+      printf("密碼長度超出限制\n");
+   else if ( len < 6)
+      printf("密碼長度太短\n");
    else
-      printf("帳號錯誤\n");
-
+      printf("建立密碼 OK\n");
 
    getchar();
    return 0;
